3584c7691d5d7098e981d0648509ca947083e21c
Mikail Gökce <66737755+themikail@users.noreply.github.com>
2021-05-04 14:18:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
+
+
+
+
+// This code was written by themikail
